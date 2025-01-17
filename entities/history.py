bf627ca13ef9c5a7b72532b4424a5d21c523b617
Gustavo Marques <gustavo.marques@uber.com>
2025-01-17 15:13:46 -0300

    Updating requirements and addition of multiple part history

--- entities/history.py
+++ entities/history.py
@@ -12,3 +12,22 @@ class History(BaseModel):
     reddit_community: str = Field(None, title="Reddit community")
     reddit_post_author: str = Field(None, title="Reddit post author")
     reddit_community_url_photo: str = Field(None, title="Reddit community url photo")
+
+
+class MultiplePartHistory(BaseModel):
+    title: str = Field("", title="Title of the history")
+    parts: List[str] = Field([], title="List of parts of the history")
+    file_name: str = Field("", title="File name of the history")
+    reddit_community: str = Field(None, title="Reddit community")
+    reddit_post_author: str = Field(None, title="Reddit post author")
+    reddit_community_url_photo: str = Field(None, title="Reddit community url photo")
+
+    def get_part(self, index) -> History:
+        return History(
+            title=self.title,
+            content=self.parts[index],
+            file_name=self.file_name,
+            reddit_community=self.reddit_community,
+            reddit_post_author=self.reddit_post_author,
+            reddit_community_url_photo=self.reddit_community_url_photo,
+        )
--- proxies/open_api_proxy.py
+++ proxies/open_api_proxy.py
@@ -1,5 +1,5 @@
 import json
-from entities.history import History
+from entities.history import History, MultiplePartHistory
 from entities.reddit import RedditPost
 from openai import OpenAI
 
@@ -37,6 +37,28 @@ HISTORY_SCHEMA = {
     },
 }
 
+MULTIPLE_PART_HISTORY_SCHEMA = {
+    "name": "multiple_part_history_schema",
+    "schema": {
+        "type": "object",
+        "properties": {
+            "title": {
+                "description": "Título da história",
+                "type": "string",
+            },
+            "parts": {
+                "description": "Lista de partes da história",
+                "type": "array",
+                "items": "string",
+            },
+            "file_name": {
+                "description": "Nome do arquivo da história sem extensão",
+                "type": "string",
+            },
+        },
+    },
+}
+
 
 def __chat_history_teller(input, schema):
     client = OpenAI()
@@ -62,12 +84,15 @@ def generate_history(prompt: str) -> History:
 
 def convert_reddit_post_to_history(reddit_post: RedditPost) -> History:
     prompt = """
-        Eu vou te passar uma história que foi postada em um fórum online. Prepare ela para narração
-        substituindo abreviaçãos pelas palavras originais e pegando trechos de entonação como "??????" e 
-        deixando em caixa alta.
-        Se a história estiver em ingês traduza mantendo o mais fiel possível.
-        O título da história vai ser passado na primeira linha, não precisa
-        adaptar ou criar um novo, apenas traduzir caso precise. 
+        Eu vou te passar uma história que foi postada em um fórum online, você deve traduzi-la e adapta-la para
+        narração. Pode substituir abreviações em ingles e português como as seguintes:
+        "I M24" -> "Eu sou um homem de 24 anos"
+        "I F20" -> "Eu sou uma mulher de 20 anos"
+        "Eu H20" -> "Eu sou um homem de 20 anos"
+        "Eu M24" -> "Eu sou um homem de 24 anos"
+
+        Faça isso com o título e a história, se estiver em ingês traduza mantendo o mais fiel possível, 
+        substituindo apenas as abreviações.
         Os demais campos, além de title e content podem ser gerados.
         Aqui está a história que você deve corrigir e traduzir:
         
@@ -80,6 +105,43 @@ def convert_reddit_post_to_history(reddit_post: RedditPost) -> History:
     )
     response = __chat_history_teller(prompt, HISTORY_SCHEMA)
     return History(
+        **response,
+        reddit_community=reddit_post.community,
+        reddit_post_author=reddit_post.author,
+        reddit_community_url_photo=reddit_post.community_url_photo,
+    )
+
+
+def convert_reddit_post_to_multiple_part_history(
+    reddit_post: RedditPost,
+    number_of_parts: int,
+) -> MultiplePartHistory:
+    prompt = """
+        Eu vou te passar uma história que foi postada em um fórum online, você deve traduzi-la e adapta-la para
+        narração, substituindo abreviações em ingles e português como as seguintes:
+        "I M24" -> "Eu sou um homem de 24 anos"
+        "I F20" -> "Eu sou uma mulher de 20 anos"
+        "Eu H20" -> "Eu sou um homem de 20 anos"
+        "Eu M24" -> "Eu sou um homem de 24 anos"
+
+        Além disso, você deve dividir a história em {number_of_parts} partes, mantendo a ordem original. E o 
+        final de cada parte deve conter um gancho para a próxima parte, alguma parte da história que deixe o
+        leitor curioso para saber o que acontece em seguida.
+
+        Se estiver em ingês traduza mantendo o mais fiel possível, 
+        substituindo apenas as abreviações.
+        Os demais campos, além de title e content podem ser gerados.
+        Aqui está a história que você deve corrigir e traduzir:
+        
+        Título: {title}
+
+        {content}
+    """.format(
+        title=reddit_post.title,
+        content=reddit_post.content,
+    )
+    response = __chat_history_teller(prompt, MULTIPLE_PART_HISTORY_SCHEMA)
+    return MultiplePartHistory(
         **response,
         reddit_community=reddit_post.community,
         reddit_post_author=reddit_post.author,
