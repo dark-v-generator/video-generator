6050b54369e506d122dfcd075c933fba9feed569
Gustavo Marques <gustavomarques.gml@gmail.com>
2025-02-08 14:17:07 -0300

    Changing speech service

--- services/speech_service.py
+++ services/speech_service.py
@@ -12,13 +12,14 @@ def synthesize_speech(
     text: str, 
     gender: VoiceGender = VoiceGender.MALE,
     rate: float = 1.0,
-) -> audio_clip.AudioClip:
+    output_path: str = "output.mp3",
+):
     if True:
         if gender == VoiceGender.MALE:
             gender = azure_proxy.VoiceVariation.MALE
         else:
             gender = azure_proxy.VoiceVariation.FEMALE
-        return azure_proxy.synthesize_speech(text, gender, rate)
+        return azure_proxy.synthesize_speech(text=text, gender=gender, rate=rate, output_path=output_path)
     elif False:
         if gender == VoiceGender.MALE:
             google_tts_gender = google_proxy.VoiceVariation.MALE
